2b98b34b8337dd49bdf868230af4d065caab2ece
AdonaiRamos2021 <91400980+AdonaiRamos2021@users.noreply.github.com>
2021-11-07 22:11:37 -0600

    Add files via upload

---
+++ AnalisisDeDatosEstudiante/src/datosestudiantes/Producto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datosestudiantes;
+
+/**
+ *
+ * @author carlo
+ */
+class Producto {
+
+    void setId(String llave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setNombre(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
